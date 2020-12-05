12e19a424303ed82a9d589d90f27e82213eb09c5
Ekirkman <edwardk110@gmail.com>
2020-12-04 20:40:35 -0600

    create recipe basic

---
+++ recipe-book/src/main/java/org/liftoff/recipebook/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package org.liftoff.recipebook.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("CreateRecipe")
+    public String createRecipe() {
+        return "CreateRecipe";
+    }
+
+
+}
