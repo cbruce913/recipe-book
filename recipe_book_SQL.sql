9bbe22653634f915f86f49b2403e8e5a0cf3094c
Ekirkman <edwardk110@gmail.com>
2021-01-28 01:20:21 -0600

    add sql

--- recipe_book_SQL.sql
+++ recipe_book_SQL.sql
@@ -1,1 +1,4 @@
 SELECT * FROM recipebook.user;
+ALTER TABLE recipe MODIFY description LONG VARCHAR;
+ALTER TABLE recipe MODIFY ingredients LONG VARCHAR;
+ALTER TABLE recipe MODIFY image_url LONG VARCHAR;
