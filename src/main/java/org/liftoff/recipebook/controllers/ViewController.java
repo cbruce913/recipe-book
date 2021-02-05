86cc873cdda84e70b570ee62ce0d019f55c47807
Carl B <c3jb@sbcglobal.net>
2021-02-05 10:39:06 -0600

    css & formatting, also added images to file but could not connect them properly yet

--- src/main/java/org/liftoff/recipebook/controllers/ViewController.java
+++ src/main/java/org/liftoff/recipebook/controllers/ViewController.java
@@ -1,6 +1,5 @@
 package org.liftoff.recipebook.controllers;
 
-import org.liftoff.recipebook.models.Recipe;
 import org.liftoff.recipebook.models.data.RecipeCategoryRepository;
 import org.liftoff.recipebook.models.data.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
-import java.util.List;
 
-/**
- * Created by LaunchCode
- */
 @Controller
 @RequestMapping(value = "view")
 public class ViewController {
 
     @Autowired
     private RecipeRepository recipeRepository;
+
     @Autowired
     private RecipeCategoryRepository recipeCategoryRepository;
 
@@ -28,7 +23,9 @@ public class ViewController {
     public String view(Model model) {
         model.addAttribute("recipes", recipeRepository.findAll());
         model.addAttribute("categories", recipeCategoryRepository.findAll());
-//        List<String> ingredientList = Arrays.asList(recipe.ingredients.split("$$"));
+/*
+        List<String> ingredientList = Arrays.asList(recipe.ingredients.split("$$"));
+*/
 
         return "view/{id}";
     }
